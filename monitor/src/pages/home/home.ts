01fb154915ce23d2277c853b56dab07f6579533f
Mauro Guardarini <mauro.guardarini@controlid.com.br>
2018-06-19 14:28:39 -0300

    menu and md-more icon

--- monitor/src/pages/home/home.ts
+++ monitor/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import "rxjs/add/operator/map";
 
 @Component({
   selector: 'page-home',
@@ -7,9 +9,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private url:string = "https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=8189426204c948f6966ee565fcbcba89";
+  public criptoMoney:JSON;
+  public price_usd:JSON;
+  public price_eur:JSON;
+  public price_real:any;
 
+  constructor(
+    public navCtrl: NavController,
+    private http: Http  
+  ) {
+
+    this.getNews(http);
     
+  }
+  public getNews(http:Http){
+   
+    this.http.get(this.url)
+    .map(res=>res.json())
+    .subscribe(data=>{
+        
+        console.log(data);
+         
+    })
+ 
   }
 
 }
